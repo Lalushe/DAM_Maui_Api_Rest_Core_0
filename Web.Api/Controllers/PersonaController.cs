54878d60548d24d5a267498f3ca8eee4a57f0e9a
Lalushe <21030196@itesa.edu.mx>
2025-06-11 11:33:46 -0600

    Version 0: Capa api, capa identidad y capa de independencia

---
+++ Web.Api/Controllers/PersonaController.cs
@@ -0,0 +1,41 @@
+﻿using CapaEntidad;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Web.Api.Models;
+
+
+namespace Web.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonaController : ControllerBase
+    {
+        [HttpGet("{nombrecompleto}")]
+        public List<PersonaCLS> listarPersona(string nombrecompleto)
+        {
+            List<PersonaCLS> lista = new List<PersonaCLS>();
+
+        try { 
+
+            using (DbAba357BdveterinariaContext bd = new DbAba357BdveterinariaContext())
+            {
+                lista = (from persona in bd.Personas
+                         where persona.Bhabilitado == 1
+                         && (persona.Nombre + " " + persona.Appaterno + " " + persona.Apmaterno).Contains(nombrecompleto)
+                         select new PersonaCLS
+                         {
+                             iidpersona = persona.Iidpersona,
+                             nombrecompleto = persona.Nombre + " " + persona.Appaterno + " " + persona.Apmaterno,
+                             correo = persona.Correo,
+                             fechanacimientocadena = persona.Fechanacimiento == null ? "" : persona.Fechanacimiento.Value.ToShortDateString(), // Convertir a cadena de fecha
+                         }).ToList();
+            }
+            return lista;
+        }
+        catch (Exception ex) 
+            {
+                return lista;
+            }
+        }
+    }
+}
