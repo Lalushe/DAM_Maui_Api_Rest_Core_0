366b27d0f2e5d289430004f0cf1f5badba7a6f56
Lalushe <21030196@itesa.edu.mx>
2025-06-12 11:56:17 -0600

    Version02: Proyecto web consumiendo Api

--- Web.Api/Controllers/PersonaController.cs
+++ Web.Api/Controllers/PersonaController.cs
@@ -2,6 +2,8 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Web.Api.Models;
+using System.Text.Json;
+
 
 
 namespace Web.Api.Controllers
@@ -10,6 +12,43 @@ namespace Web.Api.Controllers
     [ApiController]
     public class PersonaController : ControllerBase
     {
+
+        [HttpGet]
+        public List<PersonaCLS>listarPersona()
+        {
+            List<PersonaCLS> lista = new List<PersonaCLS>();
+
+            try
+            {
+
+                using (DbAba357BdveterinariaContext bd = new DbAba357BdveterinariaContext())
+                {
+                    lista = (from persona in bd.Personas
+                                   where persona.Bhabilitado == 1
+
+                                   select new PersonaCLS
+                                   {
+                                       iidpersona = persona.Iidpersona,
+                                       nombre = persona.Nombre + " " +
+                                       persona.Appaterno + " " +
+                                       persona.Apmaterno,
+                                       correo = persona.Correo,
+                                       fechanacimientocadena = persona.Fechanacimiento == null ? "" : persona.Fechanacimiento.Value.ToString("yyyy-MM-dd"), // Convertir a cadena de fecha
+                                       iidsexo = (int)persona.Iidsexo
+                                   }).ToList();
+                }
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                return lista;
+            }
+        }
+
+
+
+
+
         [HttpGet("{nombrecompleto}")]
         public List<PersonaCLS> listarPersona(string nombrecompleto)
         {
@@ -37,5 +76,90 @@ namespace Web.Api.Controllers
                 return lista;
             }
         }
+
+
+        [HttpGet("{id}")]
+        public List<PersonaCLS> listarPersona(int id)
+        {
+            List<PersonaCLS> lista = new List<PersonaCLS>();
+
+            try
+            {
+
+                using (DbAba357BdveterinariaContext bd = new DbAba357BdveterinariaContext())
+                {
+                    lista = (from persona in bd.Personas
+                             where persona.Bhabilitado == 1 && persona.Iidpersona == id
+                       
+                             select new PersonaCLS
+                             {
+                                 iidpersona = persona.Iidpersona,
+                                 nombre = persona.Nombre,
+                                 appaterno = persona.Appaterno,
+                                 apmaterno = persona.Apmaterno,
+                                 correo = persona.Correo,
+                                 fechanacimiento = (DateTime) persona.Fechanacimiento, 
+                                 fechanacimientocadena = persona.Fechanacimiento == null ? "" : persona.Fechanacimiento.Value.ToString("yyyy-MM-dd"), // Convertir a cadena de fecha
+                                 iidsexo = (int) persona.Iidsexo
+                             }).ToList();
+                }
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                return lista;
+            }
+        }
+
+
+        [HttpGet("recuperarPersona/{id}")]
+        public PersonaCLS recuperarPersona(int id)
+        {
+            PersonaCLS oPersonaCLS = new PersonaCLS();
+
+            try
+            {
+
+                using (DbAba357BdveterinariaContext bd = new DbAba357BdveterinariaContext())
+                {
+                    oPersonaCLS = (from persona in bd.Personas
+                                   where persona.Bhabilitado == 1 && persona.Iidpersona == id
+
+                                   select new PersonaCLS
+                                   {
+                                       iidpersona = persona.Iidpersona,
+                                       nombre = persona.Nombre,
+                                       appaterno = persona.Appaterno,
+                                       apmaterno = persona.Apmaterno,
+                                       correo = persona.Correo,
+                                       fechanacimiento = (DateTime)persona.Fechanacimiento,
+                                       fechanacimientocadena = persona.Fechanacimiento == null ? "" : persona.Fechanacimiento.Value.ToString("yyyy-MM-dd"), // Convertir a cadena de fecha
+                                       iidsexo = (int)persona.Iidsexo
+                                   }).First();
+                }
+                return oPersonaCLS;
+            }
+            catch (Exception ex)
+            {
+                return oPersonaCLS;
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
---
+++ Web.Cliente/Controllers/PersonaController.cs
@@ -0,0 +1,89 @@
+﻿using CapaEntidad;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Text.Json;
+using Web.Cliente.Clases;
+
+namespace Web.Cliente.Controllers
+{
+    public class PersonaController : Controller
+    {
+
+        private string urlbase;
+        private string cadena;
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public PersonaController(IConfiguration configuration, IHttpClientFactory httpClientFactory)
+        {
+            // Initialize the base URL and connection string
+            urlbase = configuration["baseurl"];
+            cadena = "HOlA";
+            _httpClientFactory = httpClientFactory;
+            
+        }
+
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        //Traer los datos o la Data como string
+        //Metodo para listar personas sin filtro
+
+        public async Task<List <PersonaCLS>> listarPersonas()
+        {
+
+          //  var cliente = _httpClientFactory.CreateClient();
+            //cliente.BaseAddress = new Uri(urlbase);
+            //string cadena = await cliente.GetStringAsync("api/Persona");
+           // List<PersonaCLS> lista = JsonSerializer.Deserialize<List<PersonaCLS>>(cadena);
+            //return lista;
+
+            return await ClientHttp.GetAll<PersonaCLS>(_httpClientFactory, urlbase, "/api/Persona");
+        }
+
+        //
+
+        //Metodo para listar personas con filtro
+        //listar
+
+        public async Task<List<PersonaCLS>> FiltrarPersonas(string nombrecompleto)
+        {
+
+            // var cliente = _httpClientFactory.CreateClient();
+            //cliente.BaseAddress = new Uri(urlbase);
+            //string cadena = await cliente.GetStringAsync("api/Persona/" + nombrecompleto);
+            //List<PersonaCLS> lista = JsonSerializer.Deserialize<List<PersonaCLS>>(cadena);
+            //return lista;
+
+            return await ClientHttp.GetAll<PersonaCLS>(_httpClientFactory, urlbase, "/api/Persona/" + nombrecompleto);
+        }
+
+
+        //Persona/FiltrarPersonas?nombrecompleto=Juan Perez
+
+
+
+        //Metodo para listar personas con filtro por ID
+
+        public async Task<PersonaCLS> recuperarPersona(int id)
+        {
+
+            // var cliente = _httpClientFactory.CreateClient();
+            //cliente.BaseAddress = new Uri(urlbase);
+            //string cadena = await cliente.GetStringAsync("api/Persona/" + nombrecompleto);
+            //List<PersonaCLS> lista = JsonSerializer.Deserialize<List<PersonaCLS>>(cadena);
+            //return lista;
+
+            return await ClientHttp.Get<PersonaCLS>(_httpClientFactory, urlbase, "/api/Persona/recuperarPersona/" + id);
+        }
+
+
+
+
+    }
+
+}
